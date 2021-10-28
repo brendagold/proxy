048d889c2030d8ca417613c6b77df799af03c068
brendagold <petersbrenda47@gmail.com>
2021-10-28 16:30:39 -0400

    added port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require("express");
 const app = express();
 
 // Configuration
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const HOST = "localhost";
 
 let url = "http://universities.hipolabs.com/search?country=Nigeria";

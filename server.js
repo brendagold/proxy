af05aa27e02ff2e1172eb124e7c53a25998c15cf
brendagold <petersbrenda47@gmail.com>
2021-11-02 04:03:22 -0400

    added header

--- server.js
+++ server.js
@@ -15,6 +15,17 @@ app.use(express.json());
 
 
 app.use(favicon(__dirname + '/public/favicon.ico'));
+
+
+// Add Access Control Allow Origin headers
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+     "Access-Control-Allow-Headers",
+     "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 // Configuration
 const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || "0.0.0.0"
